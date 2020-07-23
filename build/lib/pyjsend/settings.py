fac30cdf929fbd3a987023499486f4247040fb06
AntLouiz <luizrodrigo46@hotmail.com>
2020-07-23 16:26:08 -0300

    Fix the data on setup.py

--- build/lib/pyjsend/settings.py
+++ build/lib/pyjsend/settings.py
@@ -1,5 +1,4 @@
 import os.path
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-RESPONSES_TABLE_FILE_PATH = os.path.join(BASE_DIR, "responses_table.json")
+RESPONSES_TABLE_FILE_PATH = os.path.join(BASE_DIR, "data", "responses_table.json")
